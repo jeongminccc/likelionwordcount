584841ea56bcd969a4101ab06be8fec08d988232
jeongminccc <50401293+jeongminccc@users.noreply.github.com>
2019-06-11 22:05:15 +0900

    first

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\최정민\desktop\word_count 2\word_count\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
